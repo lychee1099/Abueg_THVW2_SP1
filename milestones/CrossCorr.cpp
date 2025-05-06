789a8b1e8ffd6c6b8e3f2600490876be969b53c6
Jezic <jezic.abueg@eee.upd.edu.ph>
2025-05-06 18:36:09 +0800

    .

--- milestones/CrossCorr.cpp
+++ milestones/CrossCorr.cpp
@@ -34,14 +34,13 @@ void CrossCorr::doCrossCorr()
     }
 
     // i think this whole loop is still O(n^2)
-    double sum;
     for (int i = 0; i < n_corr_rows; i++) {
         // needs to preserve the original DLL for the next rows
         DLL list2_flat_temp(list2_flat);
 
         corr[i].resize(n_corr_cols);
         for (int m = 0; m < n_corr_cols; m++) {
-            sum = 0.0;
+            double sum = 0.0;
             // multiply the elements of the flattened lists and accumulate
             for (int j = 0; j < list1_flat.getSize(); j++) {
                 sum += list1_flat[j] * list2_flat_temp[j];
